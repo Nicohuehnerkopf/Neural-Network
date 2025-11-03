334394204ed63d824b508aad30bcc2d132f7845b
Nicohuehnerkopf <nicolas.leufgen@icloud.com>
2025-11-03 17:02:49 +0100

    Neural Network

---
+++ list_builder.py
@@ -0,0 +1,26 @@
+import random as rn
+from settings import *
+import json
+
+weights = []
+for i in range(LAYERS):
+    if i == 0:
+        weights.append(list(list(rn.random() for j in range(NUMINPUTS)) for j in range(HIDDEN_NEURONS[i])))
+    elif i == LAYERS-1:
+        weights.append(list(list(rn.random() for j in range(HIDDEN_NEURONS[i-1])) for j in range(OUTPUTSIZE)))
+    else:
+        weights.append(list(list(rn.random() for j in range(HIDDEN_NEURONS[i-1])) for j in range(HIDDEN_NEURONS[i])))
+
+bias = []
+for i in range(LAYERS):
+    if i != LAYERS-1:
+        bias.append(list(rn.random() for j in range(HIDDEN_NEURONS[i])))
+    else:
+        bias.append(list(rn.random() for j in range(OUTPUTSIZE)))
+
+
+with open('weights.json', 'w') as f:
+    json.dump(weights, f)
+
+with open('bias.json', 'w') as f:
+    json.dump(bias, f)
